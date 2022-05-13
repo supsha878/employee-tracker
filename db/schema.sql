7e293d7835b37e072583e4e4e530489014337c7a
Supriya Sharma <sharmasup565@gmail.com>
2022-05-12 19:11:26 -0700

    fix bugs in schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,30 +4,30 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE department (
-    id INT NOT NULL,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+    id INT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT NOT NULL,
+    id INT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
-    PRIMARY KEY (id),
     FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL,
+    id INT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (role_id, manager_id)
-    REFERENCES role(id), employee(id)
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
     ON DELETE SET NULL
 );
--- index.js
+++ index.js
@@ -2,7 +2,39 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 
-// connect to database
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        user: "root",
+        password: "passwerd",
+        database: "company_db"
+    },
+    console.log(`Connected to the company_db database`)
+);
+
+function showOptions() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "option",
+                message: "What would you like to do?",
+                choices: ["view all departments", "add a department", "view all roles", "add a role", "view all employees", "add an employee", "update an employee role"]
+            }
+        ])
+        .then((data) => {
+            console.log(data.option);
+        });
+}
+
+function init() {
+    showOptions();
+}
+
+init();
+
+
+// // connect to database
 // const db = mysql.createConnection(
 //     {
 //         host: "localhost",
@@ -13,24 +45,29 @@ const mysql = require("mysql2");
 //     console.log("connected to the company_db database")
 // )
 
-// initialize app
-function init() {
-    inquirer
-        .prompt([
-            {
-                type: "list",
-                name: "action",
-                message: "Would you like to view, add, or update your information?",
-                choices: ["view", "add", "update"]
-            }
-        ])
-        .then((data) => {
-            console.log(data.action);
-        });
-}
 
-// call initialization
-init();
+
+
+// // initialize app
+// function init() {
+//     inquirer
+//         .prompt([
+//             {
+//                 type: "list",
+//                 name: "action",
+//                 message: "Would you like to view, add, or update your information?",
+//                 choices: ["view", "add", "update"]
+//             }
+//         ])
+//         .then((data) => {
+//             console.log(data.action);
+//         });
+// }
+
+// function menu()
+
+// // call initialization
+// init();
 
 /*
 - add department
